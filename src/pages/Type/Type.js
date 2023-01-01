9d640f8b9b885485abe91c21393e2849988b997e
Dharmik Abhangi <abhangidharmik@gmail.com>
2023-01-02 03:22:06 +0530

    Minor Changes

--- src/pages/Type/Type.js
+++ src/pages/Type/Type.js
@@ -1,14 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { ParaObj } from "../../Data/Paras";
 
 const Type = () => {
-  const [typedPara, setTypedPara] = useState("");
-
-  const handleTypedPara = (event) => {
-    let value = event.target.value;
-  };
+  const handleTypedPara = (event) => {};
 
   const randomParaGenerator = () => {
     let paras = Object.values(ParaObj);
@@ -16,16 +12,19 @@ const Type = () => {
     return randomPara;
   };
 
+  const handleStop = () => {};
+
   let para = randomParaGenerator();
+
   return (
     <main className="w-[100vw] h-[100vh] flex justify-center items-center">
       <Header />
-      <section className="w-5/6 h-[700px]">
+      <section className="w-5/6 min-h-[600px] max-h-[700px]">
         <div className="w-full h-[56px] flex justify-end items-center text-[#2EC4B6] font-semibold text-xl">
           Timer:
         </div>
         <div className="w-full h-[580px] flex justify-between items-center">
-          <div className="w-1/2 h-full mr-2 font-medium text-lg">{para}</div>
+          <div className="w-1/2 h-full mr-2 font-medium text-base">{para}</div>
           <div className="w-1/2 h-full ml-2 border-2 border-solid border-[#2EC4B6]">
             <textarea
               name="paraSpace"
@@ -39,7 +38,13 @@ const Type = () => {
           </div>
         </div>
         <div className="w-full h-[56px] flex justify-end items-center mt-2">
-          <button className="fontType-2 relative text-[#2EC4B6] border-2 border-solid border-[#2EC4B6] py-2 px-10 w-52 text-2xl hoverEffect">
+          <button
+            className="fontType-2 relative text-[#2EC4B6] border-2 border-solid border-[#2EC4B6] py-2 px-10 w-52 text-2xl hoverEffect2 mr-2"
+            onClick={handleStop}
+          >
+            Stop
+          </button>
+          <button className="fontType-2 relative text-[#2EC4B6] border-2 border-solid border-[#2EC4B6] py-2 px-10 w-52 text-2xl hoverEffect2">
             Reset
           </button>
         </div>
